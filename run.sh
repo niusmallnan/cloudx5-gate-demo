d51714b1c2960a3ece4d9898c32d101b55aedd08
张智博 <zhangzhibo521@gmail.com>
2016-12-04 19:25:01 +0800

    add comment

--- run.sh
+++ run.sh
@@ -1,14 +1,19 @@
 
+# Proxy
 docker run -d --name ga-proxy -p 18080:80 \
             -v `pwd`/conf.d/:/etc/nginx/conf.d/ \
             nginx
 
+# User login
 docker run -d --name login -p 10000:80 \
             -v `pwd`/login/:/usr/share/nginx/html \
             nginx
+
+# Zone A
 docker run -d --name zonea -p 10001:80 \
             -v `pwd`/zonea/:/usr/share/nginx/html \
             nginx
+# Zone B
 docker run -d --name zoneb -p 10002:80 \
             -v `pwd`/zoneb/:/usr/share/nginx/html \
             nginx
